e1bc29f95bf2ddfbc8673ccd292901ae61b51670
MundatiC <calebngethe230@gmail.com>
2023-05-15 15:55:36 +0300

    add program that prints an array

---
+++ print_array.js
@@ -0,0 +1,23 @@
+const a = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
+
+function printArray(arr) {
+   for (let i = 0; i < arr.length; i++) {
+        console.log(`row ${i}`);
+        for (let j = 0; j < arr[i].length; j++) {
+          console.log(`  ${arr[i][j]}`);
+        }
+        console.log("######");
+      }
+    return "";
+}
+
+
+console.log(printArray(a));
+
+
