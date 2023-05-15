20303d2dbfba340bae54890d4f94e47c93ce9b41
MundatiC <calebngethe230@gmail.com>
2023-05-15 16:15:15 +0300

    add program that filters some values

---
+++ filter.js
@@ -0,0 +1,10 @@
+function filter_array_values(arr) {
+    return arr.filter((value) => {
+      
+      return value !== false && value !== null && value !== 0 && value !== '';
+    });
+  }
+  
+  console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+
+  
